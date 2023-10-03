c6d3ab23272cbf61c95cd1c7459ecbcb2bbe6272
Dominik Prinzensteiner <d.prinzensteiner@gmail.com>
2023-10-03 13:26:02 +0200

    change default list

--- src/nav.js
+++ src/nav.js
@@ -1,7 +1,7 @@
 export default () => (
     {
         current: 'top',
-        items: ['new', 'top', 'best', 'show'],
+        items: ['top', 'new', 'best', 'show'],
         init() {
             let params = new URLSearchParams(window.location.search)
             this.$store.current = params.get('type') || 'top'
