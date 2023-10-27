96acd7b29d86d216caf35a015e199a424eead1e4
Dominik Prinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2023-10-27 17:38:02 +0200

    changes

--- src/item.js
+++ src/item.js
@@ -36,6 +36,8 @@ export default (initialID = null) => ({
             let element = document.createElement('div')
             element.innerHTML = data.text
             this.text = element.innerText
+        } else {
+            this.text = this.url
         }
 
         if (this.url) {
@@ -63,6 +65,7 @@ export default (initialID = null) => ({
             return
         let url = "https://hn.algolia.com/api/v1/items/:id".replace(":id", this.id)
         fetch(url).then(response => response.json()).then(data => {
+            Alpine.store('itemID', data.objectID)
             // map all fields to match hn api
             this.id = data.objectID
             this.title = data.title
@@ -71,13 +74,18 @@ export default (initialID = null) => ({
             this.by = data.author
             this.time = data.created_at_i
             this.descendants = data.children.length
-            this.text = data.text
+            if (data.text) {
+                let element = document.createElement('div')
+                element.innerHTML = data.text
+                this.text = element.innerText
+            } else {
+                this.text = this.url
+            }
             this.children = data.children
-            console.log(data)
             this.printComments(data.children, document.querySelector('#comments'))
         })
     },
-    printComments(comments, target) {
+    printComments(comments, target, hidden = false) {
         // recursively print comments for this children
         if (!comments)
             return
@@ -86,16 +94,50 @@ export default (initialID = null) => ({
             // if it has children, create new ul and call printComments
             let li = document.createElement('li')
             li.classList.add('comment', 'border-l-2', 'border-gray-200', 'dark:border-gray-700', 'pl-4', 'py-2', 'mb-2')
-            if(child.children) {
+            if (child.children.length > 0) {
                 let span = document.createElement('span')
                 span.innerHTML = unescape(child.text)
-                li.appendChild(span)
+
                 let button = document.createElement('button')
-                button.innerText = 'show replies'
-                button.classList.add('inline-block','rounded-md','px-4','py-2','text-sm','focus:relative','text-gray-500','hover:text-gray-700','dark:text-gray-400','dark:hover:text-gray-200','focus:text-white')
-                li.appendChild(button)
+                button.innerText = 'hide replies'
+                button.classList.add('ml-2','inline-block', 'rounded-md', 'px-4', 'py-2', 'text-sm', 'focus:relative', 'text-gray-500', 'hover:text-gray-700', 'dark:text-gray-400', 'dark:hover:text-gray-200', 'focus:text-white', 'bg-gray-200','dark:bg-gray-800')
+
+                let authorA = document.createElement('a')
+                authorA.href = 'https://news.ycombinator.com/user?id=' + child.author
+                authorA.target = '_blank'
+                authorA.innerText = child.author
+                authorA.classList.add('text-gray-500', 'dark:text-gray-400', 'hover:text-gray-700', 'dark:hover:text-gray-200', 'underline')
+
+                let dateSpan = document.createElement('span')
+                dateSpan.innerText = ' ' + new Date(child.created_at_i * 1000).toLocaleString()
+                dateSpan.classList.add('text-gray-500', 'dark:text-gray-400')
+
+                button.addEventListener('click', (event) => {
+                    let childrenElements = event.target.parentElement.querySelector('ul')
+                    console.log(childrenElements)
+                    if (childrenElements) {
+                        if (childrenElements.classList.contains('hidden')) {
+                            childrenElements.classList.remove('hidden')
+                            event.target.innerText = 'hide replies'
+                        } else {
+                            childrenElements.classList.add('hidden')
+                            event.target.innerText = 'show replies'
+                        }
+                    }
+                })
+
                 let ul = document.createElement('ul')
-                ul = this.printComments(child.children, ul)
+                ul = this.printComments(child.children, ul, true)
+                if (hidden) {
+                    ul.classList.add('hidden')
+                    button.innerText = 'show replies'
+                }
+
+                li.appendChild(authorA)
+                li.appendChild(dateSpan)
+                li.appendChild(button)
+                li.appendChild(document.createElement('br'))
+                li.appendChild(span)
                 li.appendChild(ul)
             } else {
                 li.innerHTML = unescape(child.text)
