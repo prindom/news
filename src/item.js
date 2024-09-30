0da3bdf4a9176b5790f54bb4e018f2dd7455b67a
dprinzensteiner <dominik.prinzensteiner@clickandlearn.at>
2024-09-30 16:42:53 +0200

    changes

--- src/item.js
+++ src/item.js
@@ -221,34 +221,35 @@ export default (initialID = null) => ({
             // if it has children, create new ul and call printComments
             let li = document.createElement('li')
             li.classList.add('comment', 'border-l-2', 'border-gray-200', 'dark:border-gray-700', 'pl-4', 'py-2', 'mb-2', 'hover:border-gray-300', 'dark:hover:border-gray-600')
+
+            let authorA = document.createElement('a')
+            authorA.href = 'https://news.ycombinator.com/user?id=' + child.author
+            authorA.target = '_blank'
+            authorA.innerText = child.author
+            authorA.classList.add('text-gray-500', 'dark:text-gray-400', 'hover:text-gray-700', 'dark:hover:text-gray-200', 'underline')
+
+            let dateSpan = document.createElement('span')
+            dateSpan.innerText = ' ' + new Date(child.created_at_i * 1000).toLocaleString()
+            dateSpan.classList.add('text-gray-500', 'dark:text-gray-400')
+
             if (child.children.length > 0) {
                 let span = document.createElement('span')
                 span.innerHTML = unescape(child.text)
 
                 let button = document.createElement('button')
-                button.innerText = 'hide replies'
-                button.classList.add('ml-2', 'inline-block', 'rounded-md', 'px-4', 'py-2', 'text-sm', 'focus:relative', 'text-gray-500', 'hover:text-gray-700', 'dark:text-gray-400', 'dark:hover:text-gray-200', 'focus:text-white', 'bg-gray-200', 'dark:bg-gray-800')
-
-                let authorA = document.createElement('a')
-                authorA.href = 'https://news.ycombinator.com/user?id=' + child.author
-                authorA.target = '_blank'
-                authorA.innerText = child.author
-                authorA.classList.add('text-gray-500', 'dark:text-gray-400', 'hover:text-gray-700', 'dark:hover:text-gray-200', 'underline')
-
-                let dateSpan = document.createElement('span')
-                dateSpan.innerText = ' ' + new Date(child.created_at_i * 1000).toLocaleString()
-                dateSpan.classList.add('text-gray-500', 'dark:text-gray-400')
-
+                button.innerText = 'collapse'
+                button.classList.add('ml-2', 'inline-block', 'rounded-md', 'px-1', 'py-1', 'text-sm', 'focus:relative', 'text-gray-500', 'hover:text-gray-700', 'dark:text-gray-400', 'dark:hover:text-gray-200', 'focus:text-white', 'bg-gray-200', 'dark:bg-gray-800')
+                
                 button.addEventListener('click', (event) => {
                     let childrenElements = event.target.parentElement.querySelector('ul')
                     console.log(childrenElements)
                     if (childrenElements) {
                         if (childrenElements.classList.contains('hidden')) {
                             childrenElements.classList.remove('hidden')
-                            event.target.innerText = 'hide replies'
+                            event.target.innerText = 'collapse'
                         } else {
                             childrenElements.classList.add('hidden')
-                            event.target.innerText = 'show replies'
+                            event.target.innerText = 'expand'
                         }
                     }
                 })
@@ -257,7 +258,7 @@ export default (initialID = null) => ({
                 ul = this.printComments(child.children, ul, true)
                 if (hidden) {
                     ul.classList.add('hidden')
-                    button.innerText = 'show replies'
+                    button.innerText = 'expand'
                 }
 
                 li.appendChild(authorA)
@@ -267,7 +268,12 @@ export default (initialID = null) => ({
                 li.appendChild(span)
                 li.appendChild(ul)
             } else {
-                li.innerHTML = unescape(child.text)
+                let span = document.createElement('span')
+                span.innerHTML = unescape(child.text)
+                li.appendChild(authorA)
+                li.appendChild(dateSpan)
+                li.appendChild(document.createElement('br'))
+                li.appendChild(span)
             }
             target.appendChild(li)
         })
