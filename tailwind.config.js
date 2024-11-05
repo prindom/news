16092c30349c95a98b6894f5ec669d60ef6ee3af
Dominik Prinzensteiner <dominik@prinzensteiner.at>
2024-11-05 22:21:09 +0100

    bugfix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // const colors = require('tailwindcss/colors')
 
-module.exports = {
+export default {
   darkMode: 'class',
   content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}', 'item.html', 'list.html'],
   theme: {
