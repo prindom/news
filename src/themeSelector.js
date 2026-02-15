a8e822ee342bf608b5af6cb1ce844e9b6b1de994
prindom <d.prinzensteiner@gmail.com>
2026-02-15 22:31:47 +0100

    add github theme

--- src/themeSelector.js
+++ src/themeSelector.js
@@ -6,6 +6,7 @@ export default () => ({
         { name: 'solarized', label: 'Solarized', color: '#268bd2' },
         { name: 'nord', label: 'Nord', color: '#88c0d0' },
         { name: 'dracula', label: 'Dracula', color: '#bd93f9' },
+        { name: 'github', label: 'GitHub', color: '#0969da' },
         { name: 'amoled', label: 'AMOLED', color: '#000000' },
     ],
 })
